0bfef8fb9926bbe1007086692f062cf94cbafa69
silfarias17 <silvifarias17@gmail.com>
2023-06-21 14:24:40 -0300

    configure middlewares, importe librerias, middleware para manejar el error 404

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
 const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(helmet());
+app.use(cors())
+app.use(morgan('tiny'));
+app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,4 +23,13 @@ app.use('/api', require('./routes/reserva.routes'));
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+//app.listen(45635, () => console.log('Server on port xxxx'));
+
+// Middleware para manejar el error 404
+app.use((req, res, next) => {
+    res.status(404).send('Error 404: Page not found');
+});
+  
+// Iniciar el servidor
+const port = 45635;
+app.listen(port, () => console.log(`Server on port ${port}`));
